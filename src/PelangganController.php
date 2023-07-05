a8357879d7d5f6601c54f7ede51ac0e7b8271afe
Arisandi Yudiarta <arisandiyudiarta21@gmail.com>
2023-07-05 16:01:23 +0800

    last commit

--- src/PelangganController.php
+++ src/PelangganController.php
@@ -68,7 +68,8 @@ class PelangganController
     {
         switch ($method){
             case "POST":
-                $data = $_POST;
+                $data = json_decode(file_get_contents("php://input"), true);
+                // $data = $_POST;
                 // var_dump($data);
                 // die;
                 $id = $this->gateway->Login($data);
